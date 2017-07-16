5cf84c3496c918a15a36c0ba8f9bb1711629a112
Pranav Gaddamadugu <pranavsaig@gmail.com>
2017-07-16 14:23:16 -0700

    sync with mam-api

--- server/mam/mam.controller.js
+++ server/mam/mam.controller.js
@@ -2,9 +2,10 @@ const path = require('path');
 const net = require('net');
 const IOTA = require('iota.lib.js');
 const generate = require('iota-generate-seed');
-const MAM = require('../lib/mam');
-const MerkleTree = require('../lib/merkle');
-const Encryption = require('../lib/encryption');
+const mam_client = require('mam.client.js');
+const MAM = mam_client.MAM;
+const MerkleTree = mam_client.Merkle;
+const Encryption = mam_client.Encryption;
 var Crypto = require('crypto.iota.js');
 const unixSocket = '/tmp/datum.sock';
 
@@ -22,83 +23,87 @@ const encryptionClient = net.createConnection({
 });
 
 const mamController = {
-  post: function(req, res) {
-      const seed = 'WLLXTOGEZXQHWAAMSLROOOIRQASUIBSVEBHDBAQGIFTJZYNMGUFMMCBTNZBWIOYJUNBWGGGLFRIXQAVHH';
-      //const seed = req.body.seed;
-      //const message = "PAUL HANDY IS HANDY";
-      const message = req.body.message;
-      const channelKeyIndex = 3;
-      const channelKey = Crypto.converter.trytes(Encryption.hash(Encryption.increment(Crypto.converter.trits(seed.slice()))));
-      const start = 3;
-      const count = 4;
-      const security = 1;
+    post: function(req, res) {
+        const seed = req.body.seed || 'WLLXTOGEZXQHWAAMSLROOOIRQASUIBSVEBHDBAQGIFTJZYNMGUFMMCBTNZBWIOYJUNBWGGGLFRIXQAVHH';
+        const message = req.body.message || (new Date()).toString();
 
-      const tree0 = new MerkleTree(seed, start, count, security);
-      const tree1 = new MerkleTree(seed, start + count, count, security);
-      let index = 0;
+        const channelKeyIndex = 3;
+        const channelKey = Crypto.converter.trytes(Encryption.hash(Encryption.increment(Crypto.converter.trits(seed.slice()))));
+        const start = 3;
+        const count = 4;
+        const security = 1;
 
-      // Get the trytes of the MAM transactions
-      const mam = new MAM.create({
-          message: iota.utils.toTrytes(message),
-          merkleTree: tree0,
-          index: index,
-          nextRoot: tree1.root.hash.toString(),
-          channelKey: channelKey,
-      });
+        const tree0 = new MerkleTree(seed, start, count, security);
+        const tree1 = new MerkleTree(seed, start + count, count, security);
+        let index = 0;
 
-      // Depth
-      const depth = 4;
+        // Get the trytes of the MAM transactions
+        const mam = new MAM.create({
+            message: iota.utils.toTrytes(message),
+            merkleTree: tree0,
+            index: index,
+            nextRoot: tree1.root.hash.toString(),
+            channelKey: channelKey,
+        });
 
-      // minWeighMagnitude
-      const minWeightMagnitude = 15;
+        // Depth
+        const depth = 4;
 
-      console.log("Next Key: " + mam.nextKey);
+        // minWeighMagnitude
+        const minWeightMagnitude = 15;
 
-      // Send trytes
-      iota.api.sendTrytes(mam.trytes, depth, minWeightMagnitude, (err, tx) => {
-        if (err)
-          console.log(err);
-        else
-          console.log(tx);
-      });
-  },
-  getMessage: function(req, res) {
-      //TODO: Send message contents in response?
-      const seed = 'WLLXTOGEZXQHWAAMSLROOOIRQASUIBSVEBHDBAQGIFTJZYNMGUFMMCBTNZBWIOYJUNBWGGGLFRIXQAVHH';
-      const channelKeyIndex = 3;
-      //const seed = req.body.seed;
+        console.log("Next Key: " + mam.nextKey);
 
-      //const channelKey = Crypto.converter.trytes(MAM.channelKey(Encryption.hash(Encryption.increment(Crypto.converter.trits(seed.slice()))), channelKeyIndex));
-      const channelKey = Crypto.converter.trytes(Encryption.hash(Encryption.increment(Crypto.converter.trits(seed.slice()))));
-      const start = 3;
-      const count = 4;
-      const security = 1;
+        // Send trytes
+        iota.api.sendTrytes(mam.trytes, depth, minWeightMagnitude, (err, tx) => {
+            if (err) {
+                console.log(err);
+                res.status(500).json({seed: seed, message: message, error: err, success: false});
+            }
+            else {
+                console.log(JSON.stringify({seed: seed, message: message, tx: tx, success: true}), null, 2);
+        res.status(200).json({seed: seed, message: message, tx: tx, success: true});
+    }
+    });
+    },
+    getMessage: function(req, res) {
+        const seed = req.body.seed || 'WLLXTOGEZXQHWAAMSLROOOIRQASUIBSVEBHDBAQGIFTJZYNMGUFMMCBTNZBWIOYJUNBWGGGLFRIXQAVHH';
+        const channelKeyIndex = 3;
+        //const channelKey = Crypto.converter.trytes(MAM.channelKey(Encryption.hash(Encryption.increment(Crypto.converter.trits(seed.slice()))), channelKeyIndex));
+        const channelKey = Crypto.converter.trytes(Encryption.hash(Encryption.increment(Crypto.converter.trits(seed.slice()))));
+        const start = 3;
+        const count = 4;
+        const security = 1;
 
-      const tree0 = new MerkleTree(seed, start, count, security);
+        const tree0 = new MerkleTree(seed, start, count, security);
 
-      console.log("channelKey: ", channelKey);
+        console.log("channelKey: ", channelKey);
 
-      const root = tree0.root.hash.toString();
-      iota.api.sendCommand({
-          command: "MAM.getMessage",
-          channel: MAM.messageID(channelKey)
-      }, function(e, result) {
-          if(e == undefined) {
-              result.ixi.map(mam => {
-                  const output = MAM.parse({key: channelKey, message: mam.message, tag: mam.tag});
-                  const asciiMessage = iota.utils.fromTrytes(output.message);
-                  if (root === output.root) {
-                      console.log("Public key match for " + root);
-                  }
-                  console.log("received: " + asciiMessage);
-              });
-
-          }
-          else {
-            console.log(e);
-          }
-      });
-  },
+        const root = tree0.root.hash.toString();
+        iota.api.sendCommand({
+            command: "MAM.getMessage",
+            channel: MAM.messageID(channelKey)
+        }, function(e, result) {
+            if(e == undefined) {
+                const rtn = {seed: seed, messages: [], success: true}
+                result.ixi.map(mam => {
+                    const output = MAM.parse({key: channelKey, message: mam.message, tag: mam.tag});
+                const asciiMessage = iota.utils.fromTrytes(output.message);
+                if (root === output.root) {
+                    console.log("Public key match for " + root);
+                }
+                rtn.messages.push(asciiMessage);
+                console.log("received: " + asciiMessage);
+            });
+                console.log(JSON.stringify(rtn, null, 2));
+                res.status(200).json(rtn);
+            }
+            else {
+                console.log(JSON.stringify({error: e, success: false}, null, 2));
+                res.status(500).json({error: e, success: false});
+            }
+        });
+    },
   sendPayment: function(req, res) {
       //TODO: Might need to reopen connection with socket server again.
       encryptionClient.write({
@@ -143,8 +148,7 @@ const mamController = {
   generateSeed: function(req, res) {
       console.log("Generating seed...")
       res.send(generate());
-  },
-
+  }
 
 }
 
