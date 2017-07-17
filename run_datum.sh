6a468810d53250a76c399be3f3c44be4c8b71939
Pranav Gaddamadugu <pranavsaig@gmail.com>
2017-07-16 20:53:40 -0700

    skeleton of shell script

--- run_datum.sh
+++ run_datum.sh
@@ -1,1 +1,12 @@
-TODO: Package Encryption Server
+#!/bin/bash
+
+#Shell script does not work
+TODO: Package Encryption Server into .jar
+
+javac ./EncryptionServer/src/main/java/com/datum/keygen/EncryptTools.java
+javac ./EncryptionServer/src/main/java/com/datum/keygen/EncryptServer.java
+
+java ./EncryptionServer/src/main/java/com/datum/keygen/EncryptTools.class
+java ./EncryptionServer/src/main/java/com/datum/keygen/EncryptServer.class
+
+node ./server/index.js
